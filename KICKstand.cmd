396953acce89c13e54843aea42fc92a898d46139
Matthew Seabrook <info@mattseabrook.net>
2023-02-25 23:36:28 -0500

    .

--- KICKstand.cmd
+++ KICKstand.cmd
@@ -13,13 +13,43 @@ if not exist %tempfolder% mkdir %tempfolder%
 
 REM Start the browser in headless mode
 if "%2" == "chrome" (
-  chrome.exe --headless --remote-debugging-port=9222 ^
+  start "" chrome.exe --headless ^
+  --remote-debugging-port=9222 ^
+  --title="KICKstand" ^
   --user-data-dir="%tempfolder%" ^
   https://www.kick.com/%1
 ) else if "%2" == "brave" (
-  brave.exe --headless --remote-debugging-port=9222 ^
+  start "" brave.exe --headless ^
+  --remote-debugging-port=9222 ^
+  --title="KICKstand" ^
   --user-data-dir="%tempfolder%" ^
   https://www.kick.com/%1
 ) else (
   echo Unknown browser specified
 )
+
+REM Wait for the headless browser to start and find the process ID using a loop
+set PID=
+set MAX_WAIT_TIME=30
+set WAIT_INTERVAL=1
+set /a WAIT_TIME=0
+:LOOP
+timeout /t %WAIT_INTERVAL% /nobreak > nul
+set /a WAIT_TIME+=WAIT_INTERVAL
+if %WAIT_TIME% geq %MAX_WAIT_TIME% (
+  echo Timed out waiting for the headless browser to start
+  exit /b
+)
+for /f "tokens=2" %%a in ('tasklist /v /fi "WINDOWTITLE eq KICKStand" /fo list ^| findstr /b "PID:"') do set PID=%%a
+if not defined PID goto LOOP
+
+
+echo The process ID of the browser is %PID%
+
+
+
+
+
+
+
+REM disable video & audio in the browser as well via command-line
