6f00e422cc52b013bf16754b37312ad713632de6
Matthew Seabrook <info@mattseabrook.net>
2023-02-26 00:47:23 -0500

    .

--- KICKstand.cmd
+++ KICKstand.cmd
@@ -28,26 +28,6 @@ if "%2" == "chrome" (
   echo Unknown browser specified
 )
 
-REM Wait for the headless browser to start and find the process ID using a loop
-set PID=
-set MAX_WAIT_TIME=30
-set WAIT_INTERVAL=1
-set /a WAIT_TIME=0
-:LOOP
-timeout /t %WAIT_INTERVAL% /nobreak > nul
-set /a WAIT_TIME+=WAIT_INTERVAL
-if %WAIT_TIME% geq %MAX_WAIT_TIME% (
-  echo Timed out waiting for the headless browser to start
-  exit /b
-)
-for /f "tokens=2" %%a in ('tasklist /v /fi "WINDOWTITLE eq KICKStand" /fo list ^| findstr /b "PID:"') do set PID=%%a
-if not defined PID goto LOOP
-
-
-echo The process ID of the browser is %PID%
-
-
-
 
 
 
